cdf7ad7da8cff32aabef837cd3d7c7936bc0ca35
lakshayvaishnav <thelakshayvaishnav@gmail.com>
2025-01-05 17:19:38 +0530

    initalized the tests with anchor-bankrun

--- project1/voting-dapp/anchor/programs/votingdapp/src/lib.rs
+++ project1/voting-dapp/anchor/programs/votingdapp/src/lib.rs
@@ -9,7 +9,21 @@ pub mod votingdapp {
 
     use super::*;
 
-    pub fn initalize_poll(_ctx: Context<InitializePoll>, _pollId: u64) -> Result<()> {
+    pub fn initalize_poll(
+        ctx: Context<InitializePoll>,
+        poll_id: u64,
+        description: String,
+        poll_start: u64,
+        poll_end: u64,
+    ) -> Result<()> {
+        let poll = &mut ctx.accounts.poll;
+        poll.poll_id = poll_id;
+        poll.description = description;
+        poll.poll_start = poll_start;
+        poll.poll_end = poll_end;
+        poll.candidate_amount = 0;
+        
+
         Ok(())
     }
 }
