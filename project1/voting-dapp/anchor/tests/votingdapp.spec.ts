0e12707033ca0a2561b630aff5ecb73f57f01197
lakshayvaishnav <thelakshayvaishnav@gmail.com>
2025-01-06 01:44:02 +0530

    all basic tests working

--- project1/voting-dapp/anchor/tests/votingdapp.spec.ts
+++ project1/voting-dapp/anchor/tests/votingdapp.spec.ts
@@ -1,10 +1,8 @@
-import * as anchor from '@coral-xyz/anchor'
 import {Program} from '@coral-xyz/anchor'
-import {Keypair, PublicKey} from '@solana/web3.js'
-
-
+import { PublicKey} from '@solana/web3.js'
 import {Votingdapp} from '../target/types/votingdapp'
 import { BankrunProvider, startAnchor } from 'anchor-bankrun'
+import * as anchor from '@coral-xyz/anchor';
 const IDL = require("../target/idl/votingdapp.json")
 
 const votingProgramAddress = new PublicKey("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF")
@@ -12,13 +10,20 @@ const votingProgramAddress = new PublicKey("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhp
 describe('votingdapp', () => {
 
   it("initializes the poll", async() => {
-    const context = await startAnchor("",[{name:"votingProgram",programId:votingProgramAddress}],[])
+    const context = await startAnchor("",[{name:"votingdapp",programId:votingProgramAddress}],[])
     const provider = new BankrunProvider(context);
 
-    const voitngProgram = new Program<Votingdapp>(
+    const votingProgram = new Program<Votingdapp>(
       IDL,
       provider
     )
-  })
-  
+
+    await votingProgram.methods.initalizePoll(
+      new anchor.BN(1),"what is your favourite flavour ?", new anchor.BN(0),new anchor.BN(1836080953)).rpc();
+
+      const [pollAdress] = PublicKey.findProgramAddressSync([new anchor.BN(1).toArrayLike(Buffer,'le',8)], votingProgramAddress)
+
+      const poll = await votingProgram.account.poll.fetch(pollAdress)
+      console.log("✅ the poll address is : ", poll);
+  },)
 })
