7a642105e17fbf87a1b5db746490c43b58200c46
lakshayvaishnav <thelakshayvaishnav@gmail.com>
2025-04-07 10:48:23 +0530

    added accounts definition for make_offer

--- project-6-swap/swap-2/programs/swap-2/src/lib.rs
+++ project-6-swap/swap-2/programs/swap-2/src/lib.rs
@@ -15,7 +15,9 @@ declare_id!("CYVNqXKnxQApda3X1V5hRCKrhFcWAkt3yxtei6zbq4ys");
 pub mod swap_2 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn make_offer(context: Context<MakeOffer>) -> Result<()> {
+    instructions::make_offer::send_offered_tokens_to_vault()?;        
+    instructions::make_offer::save_offer(context);
     }
+    
 }
