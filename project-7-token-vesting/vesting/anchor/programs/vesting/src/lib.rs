e378b4c4b993acfa98809877f352a4957e9c330f
lakshayvaishnav <thelakshayvaishnav@gmail.com>
2025-04-09 08:52:12 +0530

    added create_vesting_account and create_employee_account instructions

--- project-7-token-vesting/vesting/anchor/programs/vesting/src/lib.rs
+++ project-7-token-vesting/vesting/anchor/programs/vesting/src/lib.rs
@@ -1,7 +1,7 @@
 #![allow(clippy::result_large_err)]
 
 use anchor_lang::prelude::*;
-use anchor_spl::token_interface::{Mint, TokenAccount, TokenInterface};
+use anchor_spl::{associated_token::AssociatedToken, token_interface::{Mint, TokenAccount, TokenInterface}};
 
 declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 
@@ -11,6 +11,11 @@ pub mod vesting {
 
     pub fn create_vesting_account(ctx: Context<CreateVestingAccount>, company_name: String) -> Result<()> {
         // we are modifying the account so we are dereferencing it...
+        /*
+            Dereferencing is used when you need to manipulate the actual data stored on 
+            the reference location, less common, on the higher level
+            mostly when initializing the accounts or resetting the account data. 
+         */
         *ctx.accounts.vesting_account = VestingAccount {
             company_name: company_name,
             owner: ctx.accounts.signer.key(),
@@ -19,6 +24,46 @@ pub mod vesting {
             treasury_bump:ctx.bumps.treasury_token_account,
             bump:ctx.bumps.vesting_account
         };
+        Ok(())
+    }
+
+    pub fn create_employee_account(ctx:Context<CreateEmployeeAccount>, start_time:i64, end_time:i64, total_amount:u64, cliff_time:i64) -> Result<()> {
+        *ctx.accounts.employee_account = EmployeeAccount {
+            beneficiary: ctx.accounts.beneficiary.key(),
+            start_time: start_time,
+            end_time: end_time,
+            total_amount: total_amount,
+            total_withdrawn: 0,
+            cliff_time:cliff_time,
+            vesting_account: ctx.accounts.vesting_account.key(),
+            bump: ctx.bumps.employee_account
+        };
+        Ok(())
+    }
+
+    pub fn claim_tokens(ctx: Context<ClaimTokens>, company_name: String) -> Result<()> {
+
+        /*
+        mutable reference -  allows you to pass employee account to other functions
+        while still having the ability to modify the original data. 
+        usefule when using multiple operations.
+         */
+        let employee_account = &mut ctx.accounts.employee_account;
+
+        let now = Clock::get()?.unix_timestamp;
+
+        if now < employee_account.cliff_time {
+            // "into" method converts the error into the appropirate type for the "result"
+            return Err(ErrorCode::ClaimNotAvailableYet.into());
+        }
+
+            let time_since_start = now.saturating_sub(employee_account.start_time);
+            let total_vesting_time = employee_account.end_time.saturating_sub(employee_account.start_time);
+            
+            if total_vesting_time == 0 {
+                return Err(ErrorCode::InvalidVestingPeriod.into());
+            }
+
         Ok(())
     }
 }
@@ -50,6 +95,73 @@ pub struct CreateVestingAccount<'info> {
     pub token_program : Interface<'info, TokenInterface>
 }
 
+#[derive(Accounts)]
+pub struct CreateEmployeeAccount<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub beneficiary : SystemAccount<'info>,
+
+    #[account(
+       has_one = owner 
+    )]
+    pub vesting_account : Account<'info, VestingAccount>,
+
+    #[account(
+        init,
+        space = 8 + EmployeeAccount::INIT_SPACE,
+        payer = owner,
+        seeds = [b"employee_vesting", beneficiary.key().as_ref(), vesting_account.key().as_ref()],
+        bump,  
+    )]
+    pub employee_account : Account<'info, EmployeeAccount>,
+    
+    pub system_program : Program<'info, System>
+    
+}
+
+#[derive(Accounts)]
+#[instruction(company_name:String)]
+pub struct ClaimTokens<'info> {
+    #[account(mut)]
+    pub beneficiary: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"employee_vesting", beneficiary.key().as_ref(), vesting_account.key().as_ref()],
+        bump = employee_account.bump,
+        has_one = beneficiary,
+        has_one = vesting_account
+    )]
+    pub employee_account: Account<'info, EmployeeAccount>,
+
+    #[account(
+        mut,
+        seeds = [company_name.as_ref()],
+        bump = vesting_account.bump,
+        has_one = treasury_token_account,
+        has_one = mint,
+    )]
+    pub vesting_account : Account<'info,VestingAccount >,
+
+    pub mint : InterfaceAccount<'info, Mint>, 
+
+    #[account(mut)]
+    pub treasury_token_account : InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        init_if_needed,
+        payer = beneficiary,
+        associated_token::mint = mint,
+        associated_token::authority = beneficiary,
+        associated_token::token_program = token_program
+    )]
+    pub employee_token_account : InterfaceAccount<'info, TokenAccount>,
+    pub token_program : Interface<'info, TokenInterface>,
+    pub associated_token_program : Program<'info, AssociatedToken>,
+    pub system_program : Program<'info, System>
+
+ }
+
 #[account]
 #[derive(InitSpace)]
 pub struct VestingAccount {
@@ -60,4 +172,26 @@ pub struct VestingAccount {
     pub company_name: String,
     pub treasury_bump: u8,
     pub bump: u8,
+}
+
+
+#[account]
+#[derive(InitSpace)]
+pub struct EmployeeAccount {
+    pub beneficiary : Pubkey,
+    pub start_time: i64,
+    pub end_time: i64,
+    pub cliff_time: i64,
+    pub vesting_account: Pubkey,
+    pub total_amount: u64,
+    pub total_withdrawn: u64,
+    pub bump: u8
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("claim not available yet")]
+    ClaimNotAvailableYet,
+    #[msg("Invalid vesting period")]
+    InvalidVestingPeriod
 }
