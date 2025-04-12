a9d9943394ca3831c28dcec839988be26ee53f21
lakshayvaishnav <thelakshayvaishnav@gmail.com>
2025-04-12 11:38:10 +0530

    initialized project - 8

---
+++ project-8-lottery/token-lottery/anchor/programs/tokenlottery/src/lib.rs
@@ -0,0 +1,12 @@
+#![allow(clippy::result_large_err)]
+
+use anchor_lang::prelude::*;
+
+declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
+
+#[program]
+pub mod tokenlottery {
+    use super::*;
+
+ 
+}
