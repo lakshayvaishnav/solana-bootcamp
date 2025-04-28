83c554558659eb2afafb44d160d8e1f4d310b96a
laksayvaishnav <thelakshayvaishnav@gmail.com>
2025-04-28 07:19:23 +0000

    added user state

--- project-10-lending/lending/programs/lending/src/state.rs
+++ project-10-lending/lending/programs/lending/src/state.rs
@@ -3,16 +3,33 @@ use anchor_lang::prelude::*;
 #[account]
 #[derive(InitSpace)]
 pub struct Bank {
-    pub authority: Pubkey, // to make changes to bank state
-    pub mint_address: Pubkey, // mint address of the asset
-    pub total_deposits: u64, // current number of tokens in the bank
-    pub total_deposit_shares: u64, // current number of deposit shares in bank
-    pub total_borrowed: u64, // current number of borowwed tokens in the bank
-    pub total_borrowed_shares: u64, // current borowwed shares in the bank
+    pub authority: Pubkey,             // to make changes to bank state
+    pub mint_address: Pubkey,          // mint address of the asset
+    pub total_deposits: u64,           // current number of tokens in the bank
+    pub total_deposit_shares: u64,     // current number of deposit shares in bank
+    pub total_borrowed: u64,           // current number of borowwed tokens in the bank
+    pub total_borrowed_shares: u64,    // current borowwed shares in the bank
     pub liquidation_threshold: u64, // ltv at which the loan is defined as under collaterized and can be liquidated
-    pub liquidation_bonus: u64, // bonus percentage of collateral that can be liquidated
+    pub liquidation_bonus: u64,     // bonus percentage of collateral that can be liquidated
     pub liquidation_close_factor: u64, // percentage of collateral that can be liquidated
-    pub max_lt: u64, // max % of collaterat that can be borowwed.
-    pub last_updated: i64, // timestamp
+    pub max_lt: u64,                // max % of collaterat that can be borowwed.
+    pub last_updated: i64,          // timestamp
     pub interest_rate: u64,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct User {
+    pub owner: Pubkey,              // user's wallet
+    pub deposited_sol: u64,         // deposited token in the sol bank
+    pub deposited_sol_shares: u64,  // user's deposited share in sol bank
+    pub borrowed_sol: u64,          // user's borrowed tokens in the sol bank
+    pub borrowed_sol_shares: u64,   // user's borrowed shares
+    pub deposited_usdc: u64,        // deposited tokens in the usdc token
+    pub deposited_usdc_shares: u64, // deposited shares in the usdc bank
+    pub borrowed_usdc: u64,         // borrowed usdc in the usdc bank
+    pub borrowed_usdc_shares: u64,  //...
+    pub usdc_addres: Pubkey,        // usdc mint address
+    pub health_factor: u64,         // current health factor of the user
+    pub last_updated: i64,          // timestamp
 }
