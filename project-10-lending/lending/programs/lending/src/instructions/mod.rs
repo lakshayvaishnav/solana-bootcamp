ca067753de8281f5d25c88578ecab56e5c5aef49
laksayvaishnav <thelakshayvaishnav@gmail.com>
2025-05-31 18:21:11 +0000

    done with repay struct

--- project-10-lending/lending/programs/lending/src/instructions/mod.rs
+++ project-10-lending/lending/programs/lending/src/instructions/mod.rs
@@ -1,9 +1,11 @@
 pub mod admin;
 pub mod borrow;
 pub mod deposit;
+pub mod repay;
 pub mod withdraw;
 
 pub use admin::*;
 pub use borrow::*;
 pub use deposit::*;
+pub use repay::*;
 pub use withdraw::*;
