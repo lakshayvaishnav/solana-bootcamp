b83226895a25b2c2464ab58f031e3fa3b90618c0
laksayvaishnav <thelakshayvaishnav@gmail.com>
2025-06-11 21:27:54 +0000

    added states

--- project-11-stable-coin/stablecoin/programs/stablecoin/src/lib.rs
+++ project-11-stable-coin/stablecoin/programs/stablecoin/src/lib.rs
@@ -1,16 +1,8 @@
+pub mod state;
+
+pub use state::*;
+
 use anchor_lang::prelude::*;
 
-declare_id!("7p5mRD4WXczDvTFao7h2rGnT8JbEK4DpxetaFQzk99j1");
+declare_id!("7p5mRD4WXczDvTFao7h2rGnT8JbEK5DpxetaFQzk99j1");
 
-#[program]
-pub mod stablecoin {
-    use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
-    }
-}
-
-#[derive(Accounts)]
-pub struct Initialize {}
