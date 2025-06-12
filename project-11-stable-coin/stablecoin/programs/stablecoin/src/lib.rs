34256c7efd7b06e7e443fdd8b4d85370e9c55e2d
laksayvaishnav <thelakshayvaishnav@gmail.com>
2025-06-12 21:48:58 +0000

    added liquidate ixn

--- project-11-stable-coin/stablecoin/programs/stablecoin/src/lib.rs
+++ project-11-stable-coin/stablecoin/programs/stablecoin/src/lib.rs
@@ -30,7 +30,7 @@ pub mod stablecoin {
     pub fn deposit_collateral_mint_tokens(
         ctx: Context<DespositCollateralMintTokens>,
         amount_collateral: u64,
-        amount_to_mint: u64,
+        amount_to_mint: u64
     ) -> Result<()> {
         process_deposit_collateral_and_mint_tokens(ctx, amount_collateral, amount_to_mint)
     }
@@ -38,8 +38,12 @@ pub mod stablecoin {
     pub fn reddem_collateral_and_burn_tokens(
         ctx: Context<RedeemCollateralAndBurnTokens>,
         amount_collateral: u64,
-        amount_to_burn: u64,
+        amount_to_burn: u64
     ) -> Result<()> {
         process_redeem_collateral_and_burn_tokens(ctx, amount_collateral, amount_to_burn)
+    }
+
+    pub fn liquidate(ctx: Context<Liquidate>, amount_to_burn: u64) -> Result<()> {
+        process_liquidate(ctx, amount_to_burn)
     }
 }
