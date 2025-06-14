1795765f169bb3807919bc6a2bad8d788ec9147d
laksayvaishnav <thelakshayvaishnav@gmail.com>
2025-06-14 00:13:41 +0000

    added cpi using signer seeds

--- spltoken/token/programs/token/src/lib.rs
+++ spltoken/token/programs/token/src/lib.rs
@@ -15,35 +15,55 @@ pub mod token {
 
     use super::*;
 
-    pub fn create_mint(ctx: Context<CreateMint>) -> Result<()> {
-        msg!("createdd mint account : {:?}", ctx.accounts.mint.key());
-        Ok(())
-    }
+    // pub fn create_mint(ctx: Context<CreateMint>) -> Result<()> {
+    //     msg!("createdd mint account : {:?}", ctx.accounts.mint.key());
+    //     Ok(())
+    // }
 
     pub fn create_mint_pda(ctx: Context<CreateMintPDA>) -> Result<()> {
         msg!("created mint account using pda : {:?}", ctx.accounts.mint.key());
         Ok(())
     }
 
-    pub fn create_token_account(ctx: Context<CreateTokenAccount>) -> Result<()> {
-        msg!("token account created successfully : {:?}", ctx.accounts.token_account.key());
-        Ok(())
-    }
+    // pub fn create_token_account(ctx: Context<CreateTokenAccount>) -> Result<()> {
+    //     msg!("token account created successfully : {:?}", ctx.accounts.token_account.key());
+    //     Ok(())
+    // }
 
-    pub fn mint_tokens(ctx: Context<MintTokens>, amount: u64) -> Result<()> {
+    // pub fn mint_tokens(ctx: Context<MintTokens>, amount: u64) -> Result<()> {
+    //     let cpi_accounts = MintTo {
+    //         authority: ctx.accounts.signer.to_account_info(),
+    //         mint: ctx.accounts.mint.to_account_info(),
+    //         to: ctx.accounts.token_account.to_account_info(),
+    //     };
+
+    //     let cpi_program = ctx.accounts.token_program.to_account_info();
+
+    //     let cpi_context = CpiContext::new(cpi_program, cpi_accounts);
+    //     token_interface::mint_to(cpi_context, amount)?;
+
+    //     Ok(())
+    // }
+
+    pub fn mint_tokens_pda(ctx:Context<MintTokensPDA>, amount: u64) -> Result<()> {
+        let signer_seeds: &[&[&[u8]]] = &[&[b"mint", &[ctx.bumps.mint]]];
+
         let cpi_accounts = MintTo {
-            authority: ctx.accounts.signer.to_account_info(),
-            mint: ctx.accounts.mint.to_account_info(),
-            to: ctx.accounts.token_account.to_account_info(),
+                authority: ctx.accounts.mint.to_account_info(),
+                mint: ctx.accounts.mint.to_account_info(),
+                to: ctx.accounts.token_account.to_account_info(),
         };
 
-        let cpi_program = ctx.accounts.token_account.to_account_info();
+        let cpi_program = ctx.accounts.token_program.to_account_info();
 
-        let cpi_context = CpiContext::new(cpi_program, cpi_accounts);
+        let cpi_context = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
         token_interface::mint_to(cpi_context, amount)?;
 
         Ok(())
+
     }
+
+    // pub fn mint_tokens_pda()
 }
 
 /*
@@ -51,22 +71,22 @@ A mint account is an account type in Solana's Token Programs that uniquely repre
 a token on the network and stores global metadata about the token.
 */
 
-#[derive(Accounts)]
-pub struct CreateMint<'info> {
-    #[account(mut)]
-    pub signer: Signer<'info>,
+// #[derive(Accounts)]
+// pub struct CreateMint<'info> {
+//     #[account(mut)]
+//     pub signer: Signer<'info>,
 
-    #[account(
-        init,
-        payer = signer,
-        mint::decimals = 6,
-        mint::authority = signer.key(),
-        mint::freeze_authority = signer.key()
-    )]
-    pub mint: InterfaceAccount<'info, Mint>,
-    pub token_program: Interface<'info, TokenInterface>,
-    pub system_program: Program<'info, System>,
-}
+//     #[account(
+//         init,
+//         payer = signer,
+//         mint::decimals = 6,
+//         mint::authority = signer.key(),
+//         mint::freeze_authority = signer.key()
+//     )]
+//     pub mint: InterfaceAccount<'info, Mint>,
+//     pub token_program: Interface<'info, TokenInterface>,
+//     pub system_program: Program<'info, System>,
+// }
 
 #[derive(Accounts)]
 pub struct CreateMintPDA<'info> {
@@ -77,9 +97,9 @@ pub struct CreateMintPDA<'info> {
         init,
         payer = signer,
         mint::decimals = 6,
-        mint::authority = signer.key(),
-        mint::freeze_authority = signer.key(),
-        seeds = [b"mint", signer.key().as_ref()],
+        mint::authority = mint,
+        mint::freeze_authority = mint,
+        seeds = [b"mint"],
         bump
     )]
     pub mint: InterfaceAccount<'info, Mint>,
@@ -97,10 +117,50 @@ ATAs are recommended for user wallets, while custom token accounts are useful
 for program controlled accounts
 */
 
+// #[derive(Accounts)]
+// pub struct CreateTokenAccount<'info> {
+//     #[account(mut)]
+//     pub signer: Signer<'info>,
+//     #[account(
+//         init_if_needed,
+//         payer = signer,
+//         associated_token::mint = mint,
+//         associated_token::authority = signer,
+//         associated_token::token_program = token_program
+//     )]
+//     pub token_account: InterfaceAccount<'info, TokenAccount>,
+//     pub token_program: Interface<'info, TokenInterface>,
+//     pub mint: InterfaceAccount<'info, Mint>,
+//     pub associated_token_program: Program<'info, AssociatedToken>,
+//     pub system_program: Program<'info, System>,
+// }
+
+// #[derive(Accounts)]
+// pub struct MintTokens<'info> {
+//     #[account(mut)]
+//     pub signer: Signer<'info>,
+
+//     #[account(mut)]
+//     pub mint: InterfaceAccount<'info, Mint>,
+
+//     #[account(mut)]
+//     pub token_account: InterfaceAccount<'info, TokenAccount>,
+
+//     pub token_program: Interface<'info, TokenInterface>,
+// }
+
 #[derive(Accounts)]
-pub struct CreateTokenAccount<'info> {
+pub struct MintTokensPDA<'info> {
     #[account(mut)]
     pub signer: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds=[b"mint"],
+        bump
+    )]
+    pub mint: InterfaceAccount<'info, Mint>,
+
     #[account(
         init_if_needed,
         payer = signer,
@@ -109,22 +169,7 @@ pub struct CreateTokenAccount<'info> {
         associated_token::token_program = token_program
     )]
     pub token_account: InterfaceAccount<'info, TokenAccount>,
-    pub token_program: Interface<'info, TokenInterface>,
-    pub mint: InterfaceAccount<'info, Mint>,
-    pub associated_token_program: Program<'info, AssociatedToken>,
     pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-pub struct MintTokens<'info> {
-    #[account(mut)]
-    pub signer: Signer<'info>,
-
-    #[account(mut)]
-    pub mint: InterfaceAccount<'info, Mint>,
-
-    #[account(mut)]
-    pub token_account: InterfaceAccount<'info, TokenAccount>,
-
     pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
 }
--- spltoken/token/tests/token.ts
+++ spltoken/token/tests/token.ts
@@ -2,6 +2,8 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Token } from "../target/types/token";
 import { Keypair } from "@solana/web3.js";
+import { TOKEN_PROGRAM_ID } from "@coral-xyz/anchor/dist/cjs/utils/token";
+import { BN } from "bn.js";
 
 describe("token", () => {
 
@@ -11,32 +13,51 @@ describe("token", () => {
   const program = anchor.workspace.token as Program<Token>;
   const wallet = provider.wallet;
 
-  it("it initalizes mint account ", async () => {
+  // it("it initalizes mint account ", async () => {
 
 
-    const tx = await program.methods.createMint().accounts({
-      mint: mintKeypair.publicKey,
-      signer: wallet.publicKey
-    })
-      .signers([mintKeypair])
-      .rpc({ commitment: "confirmed" })
+  //   const tx = await program.methods.createMint().accounts({
+  //     mint: mintKeypair.publicKey,
+  //     signer: wallet.publicKey,
+  //     tokenProgram: TOKEN_PROGRAM_ID
+  //   })
+  //     .signers([mintKeypair])
+  //     .rpc({ commitment: "confirmed" })
 
-    console.log(" ✅here is the signature : ", tx)
+  //   console.log(" ✅here is the signature : ", tx)
 
-  });
+  // });
 
-  it("it creates mint account using pda ", async () => {
+  // it("it creates mint account using pda ", async () => {
+  //   const tx = await program.methods.createMintPda().accounts({
+  //     signer: wallet.publicKey,
+  //     tokenProgram: TOKEN_PROGRAM_ID
+  //   }).rpc({ commitment: "confirmed" })
+
+  //   console.log("✅ your signature : ", tx)
+  // })
+
+  // it("it creates token account :", async () => {
+  //   const tx = await program.methods.createTokenAccount().accounts({
+  //     signer: wallet.publicKey,
+  //     mint: mintKeypair.publicKey
+  //   }).rpc({ commitment: "confirmed" })
+  // })
+
+  it ("creates mint account using pda ", async() => {
     const tx = await program.methods.createMintPda().accounts({
       signer: wallet.publicKey,
-    }).rpc({ commitment: "confirmed" })
+      tokenProgram: TOKEN_PROGRAM_ID
+    }).rpc({commitment:"confirmed"})
 
-    console.log("✅ your signature : ", tx)
+    console.log("✅ mint account created as pda : ", tx);
   })
 
-  it("it creates token account :", async () => {
-    const tx = await program.methods.createTokenAccount().accounts({
+  it("mint tokens as pda using signer seeds ", async() => {
+    const tx = await program.methods.mintTokensPda(new BN(100)).accounts({
       signer: wallet.publicKey,
-      mint: mintKeypair.publicKey
-    }).rpc({ commitment: "confirmed" })
+      tokenProgram: TOKEN_PROGRAM_ID
+    }).rpc({commitment:"confirmed"})
   })
+
 });
