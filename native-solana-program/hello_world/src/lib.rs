bb1c0dd3fc14e8b61511fefb6de62e7e94bbafdf
laksayvaishnav <thelakshayvaishnav@gmail.com>
2025-06-21 23:41:59 +0000

    added test in rusts using solana-porgram-test solana-sdk and tokio

--- native-solana-program/hello_world/src/lib.rs
+++ native-solana-program/hello_world/src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(warnings)]
+
 use solana_program::{
     account_info::AccountInfo,
     entrypoint,
@@ -15,4 +17,41 @@ pub fn process_instruction(
 ) -> ProgramResult {
     msg!("hello world");
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use std::time::Instant;
+
+    use solana_program_test::*;
+    use solana_sdk::{
+        instruction::Instruction,
+        pubkey::Pubkey,
+        signature::Signer,
+        transaction::Transaction,
+    };
+
+    #[tokio::test]
+    async fn test_hello_world() {
+        let program_id = Pubkey::new_unique();
+        let mut program_test = ProgramTest::default();
+        program_test.add_program("hello_world", program_id, None);
+        let (mut banks_client, payer, recent_blockhash) = program_test.start().await;
+
+        // create instructions
+        let instruction = Instruction {
+            program_id,
+            accounts: vec![],
+            data: vec![],
+        };
+
+        // create transaction with instruction
+        let mut transaction = Transaction::new_with_payer(&[instruction], Some(&payer.pubkey()));
+
+        // sign the transaction
+        transaction.sign(&[&payer], recent_blockhash);
+
+        let transaction_result = banks_client.process_transaction(transaction).await;
+        assert!(transaction_result.is_ok());
+    }
 }
