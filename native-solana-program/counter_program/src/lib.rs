1e90fc4d20d4e4fd2489077b9858dd24d1fb7095
laksayvaishnav <thelakshayvaishnav@gmail.com>
2025-06-23 04:53:52 +0000

    added increase counter ixn

--- native-solana-program/counter_program/src/lib.rs
+++ native-solana-program/counter_program/src/lib.rs
@@ -1,7 +1,7 @@
 use borsh::{ BorshDeserialize, BorshSerialize };
 use solana_program::{
     account_info::{ next_account_info, AccountInfo },
-    entrypoint:: ProgramResult ,
+    entrypoint::ProgramResult,
     entrypoint,
     program::invoke,
     program_error::ProgramError,
@@ -95,6 +95,34 @@ fn process_initialize_counter(
     Ok(())
 }
 
+/*
+Note that in practice, a developer must implement various security checks to validate the accounts passed to the program.
+ Since all accounts are provided by the caller of the instruction,
+ there is no guarantee that the accounts provided are the ones the program expects. 
+ Missing account validation checks are a common source of program vulnerabilities.
+
+*/
+
+// adding check to ensure the account refferring to is owendy by the executing program
 fn process_increment_counter(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult {
+    let accounts_iter = &mut accounts.iter();
+    let counter_account = next_account_info(accounts_iter)?;
+
+    // verify account ownership
+    if counter_account.owner != program_id {
+        return Err(ProgramError::IncorrectProgramId);
+    }
+
+    // borrow the account data mutabily
+    let mut data = counter_account.data.borrow_mut();
+
+    // Deserialize the account data into our counteraccount struct
+    let mut counter_data = CounterAccount::try_from_slice(&data)?;
+    counter_data.count = counter_data.count.checked_add(1).ok_or(ProgramError::InvalidAccountData)?;
+
+    counter_data.serialize(&mut &mut data[..])?;
+
+    msg!("Counter incremented to : {}", counter_data.count);
+
     Ok(())
 }
