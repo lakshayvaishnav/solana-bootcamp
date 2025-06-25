d35ac12b14bb6968d768c624dcf8984f47d8eb3e
laksayvaishnav <thelakshayvaishnav@gmail.com>
2025-06-25 11:39:27 +0000

    added sealed and is_initialized trait to state

--- native-solana-program/movie_review/src/state.rs
+++ native-solana-program/movie_review/src/state.rs
@@ -1,4 +1,5 @@
 use borsh::{ BorshDeserialize, BorshSerialize };
+use solana_program::program_pack::{IsInitialized, Sealed};
 
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct MovieAccountState {
@@ -6,4 +7,17 @@ pub struct MovieAccountState {
     pub title: String,
     pub description: String,
     pub rating: u8,
+}
+
+/*
+    Sealed is Solana's version of Rust's Sized trait. 
+    This simply specifies that MovieAccountState has a known size and 
+    provides for some compiler optimizations.
+*/
+impl Sealed for MovieAccountState {}
+
+impl IsInitialized for MovieAccountState {
+    fn is_initialized(&self) -> bool {
+        self.is_initialized
+    }
 }
