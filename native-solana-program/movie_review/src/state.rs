6e9763431616b3ecaf1a8a6adb8858a24f59d96d
laksayvaishnav <thelakshayvaishnav@gmail.com>
2025-06-26 11:53:03 +0000

    updated the states for movie comment

--- native-solana-program/movie_review/src/state.rs
+++ native-solana-program/movie_review/src/state.rs
@@ -1,12 +1,31 @@
 use borsh::{ BorshDeserialize, BorshSerialize };
-use solana_program::program_pack::{IsInitialized, Sealed};
+use solana_program::{ program_pack::{ IsInitialized, Sealed }, pubkey::Pubkey };
 
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct MovieAccountState {
+    pub descriminator: String,
     pub is_initialized: bool,
+    pub reviewer: Pubkey,
     pub title: String,
     pub description: String,
     pub rating: u8,
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct MovieCommentCounter {
+    pub discriminator: String,
+    pub is_initialized: bool,
+    pub counter: u64,
+}
+
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct MovieComment {
+    pub discriminator: String,
+    pub is_initialized: bool,
+    pub review: Pubkey,
+    pub commenter: Pubkey,
+    pub comment: String,
+    pub count: u64,
 }
 
 /*
@@ -19,5 +38,45 @@ impl Sealed for MovieAccountState {}
 impl IsInitialized for MovieAccountState {
     fn is_initialized(&self) -> bool {
         self.is_initialized
+    }
+}
+
+impl IsInitialized for MovieCommentCounter {
+    fn is_initialized(&self) -> bool {
+        self.is_initialized
+    }
+}
+
+impl IsInitialized for MovieComment {
+    fn is_initialized(&self) -> bool {
+        self.is_initialized
+    }
+}
+
+impl MovieAccountState {
+    pub const DISCRIMINATOR: &'static str = "review";
+
+    pub fn get_account_size(title: String, description: String) -> usize {
+        return (
+            4 +
+            MovieAccountState::DISCRIMINATOR.len() +
+            1 +
+            1 +
+            (4 + title.len()) +
+            (4 + description.len())
+        );
+    }
+}
+
+impl MovieCommentCounter {
+    pub const DISCRIMINATOR: &'static str = "counter";
+    pub const SIZE: usize = 4 + MovieCommentCounter::DISCRIMINATOR.len() + 1 + 8;
+}
+
+impl MovieComment {
+    pub const DISCRIMINATOR: &'static str = "comment";
+
+    pub fn get_account_size(comment: String) -> usize {
+        return 4 + MovieComment::DISCRIMINATOR.len() + 1 + 32 + 32 + (4 + comment.len()) + 8;
     }
 }
